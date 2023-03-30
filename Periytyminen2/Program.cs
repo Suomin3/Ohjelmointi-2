edae0ee1aede18e8bc331f62bbbd4dd266bcf8a2
Tapio Suominen <suominen.tapio@outlook.com>
2023-03-30 10:16:29 +0300

    blyat

--- Periytyminen2/Program.cs
+++ Periytyminen2/Program.cs
@@ -1,45 +1,80 @@
-﻿
-class Koira {
-    public string nimi;
-    public bool murisee = false;
-    public string Juo (){
-        return "lurps lurps";
-    }
-    public string Syo (){
-        return "nom nom";
-    }
-    public string Haukkuu (){
-        return "vuh";
-    }
-}
-class Koiranpentu : Koira{
-    public bool pieni;
-}
-class Susi : Koira{
-    bool peloton;
+﻿namespace T5_koodit;
 
-    public Susi(bool _peloton){
-        peloton = _peloton;
-    Console.WriteLine("Susi on peloton = " + peloton);
+class Program{
+    static void Main(string[] args){
+
+    Peruspertti peruspertti1 = new Peruspertti();
+    peruspertti1.LuoKayttaja1("Peruspertti","salasana1");
+
+    // Admin admin1 = new Admin();
+    // admin1.LuoKayttaja2("Admin","salasana2");
+
+    Console.Write("Anna käyttäjätunnus: ");
+    string kt = Console.ReadLine();
+    Console.Write("Anna salasana: ");
+    string ss = Console.ReadLine();
+
+    peruspertti1.TestaaKayttaja1(kt, ss);
+    // admin1.TestaaKayttaja2(kt, ss);
     }
 }
 
-class Program
-{
-    public static void Main(string[] args)
-    {
-        Koira koira1 = new Koira();
-        koira1.nimi = "Musti";
-        Console.WriteLine($"{koira1.nimi} syö " + koira1.Syo());
+class Kayttaja{
+    static string kayttajatunnus1 = "Peruspertti";
+    string salasana1 = "salasana1";
+    static string kayttajatunnus2 = "Admin";
+    string salasana2 = "salasana2";
 
-        Koiranpentu pentu1 = new Koiranpentu();
-        pentu1.nimi = "Regina";
-        pentu1.pieni = false;
-        Console.WriteLine($"{pentu1.nimi} juo " + pentu1.Juo());
-
-        Susi susi1 = new Susi(true);
-        susi1.nimi = "Jyrki";
-        // susi1.peloton = true;
-        Console.WriteLine($"{susi1.nimi} haukkuu " + susi1.Haukkuu());
+    protected void LuoKayttaja(string _kayttajatunnus, string _salasana){
+        kayttajatunnus1 = _kayttajatunnus;
+        salasana1 = _salasana;
+        Console.WriteLine("Käyttäjä {0} luotu", kayttajatunnus1);
+    }
+    protected void Ohje(){
+        Console.WriteLine("Ohjeet tulossa...");
+    }
+    protected virtual void NaytaKayttaja(){
+        string kayttajatunnusVain3 = kayttajatunnus1.Substring(kayttajatunnus1.Length - 3);
+        Console.WriteLine("--Käyttäjä--");
+        Console.WriteLine($"Käyttäjänimi loppuu: {kayttajatunnusVain3}");
+        Console.WriteLine("Salasana: ***");
+    }
+    protected void TestaaKayttaja(string kt, string ss){
+        if(kt == kayttajatunnus1 && ss == salasana1){
+            Console.WriteLine("Kirjautuminen onnistui!");
+            // NaytaKayttaja();
+            // Ohje();
+        }else if(kt == kayttajatunnus2 && ss == salasana2){
+            Console.WriteLine("Kirjautuminen onnistui!");
+            // NaytaKayttaja();
+        }else{
+            Console.WriteLine("Kirjautuminen epäonnistui!");
+        }
     }
 }
+
+
+
+class Peruspertti : Kayttaja{
+    public void LuoKayttaja1(string _kayttajatunnus, string _salasana){
+        LuoKayttaja(_kayttajatunnus, _salasana);
+    }
+    public void TestaaKayttaja1(string kt, string ss){
+        TestaaKayttaja(kt, ss);
+    }
+}
+
+// class Admin : Kayttaja{
+//     public void LuoKayttaja2(string _kayttajatunnus, string _salasana){
+//         LuoKayttaja(_kayttajatunnus, _salasana);
+//     }
+//     public void TestaaKayttaja2(string kt, string ss){
+//         TestaaKayttaja(kt, ss);
+//     }
+//     protected override void NaytaKayttaja(){
+//         // string kayttajatunnusVain3 = kayttajatunnus2.Substring(kayttajatunnus2.Length - 3);
+//             Console.WriteLine("--Käyttäjä--");
+//             Console.WriteLine("Käyttäjätunnus: Admin");
+//             Console.WriteLine("Salasana: salasana2");
+//     }
+// }
--- Periytyminen3/Program.cs
+++ Periytyminen3/Program.cs
@@ -1,34 +1,45 @@
-﻿// namespace Leffat;
+﻿
+class Koira {
+    public string nimi;
+    public bool murisee = false;
+    public string Juo (){
+        return "lurps lurps";
+    }
+    public string Syo (){
+        return "nom nom";
+    }
+    public string Haukkuu (){
+        return "vuh";
+    }
+}
+class Koiranpentu : Koira{
+    public bool pieni;
+}
+class Susi : Koira{
+    bool peloton;
 
-// class Genre {
-//     public string _Genre1 (){
-//         return "Drama";
-    
-//     bool K18;
-//     }
-//     public K18(bool _K18){
-//         K18 = _K18;
-// }
-// class Movie {
-//     public string Name (){
-//         return "Forrest Gump";
-//     }
-//     public int Year (){
-//         return 1994;
-//     }
-//     public string Director (){
-//         return "Robert Zemeckis";
-//     }
-// }
-// class Program{
-//     public static void Main(string[] args)
-//     {
-//         Genre genre1 = new Genre(false);
-//         if(
-//             K18 == false
-//             Console.WriteLine("Elokuva on ikärajaton");
-//         )
-//         Console.WriteLine(genre1._Genre1());
-//     }
-// }
+    public Susi(bool _peloton){
+        peloton = _peloton;
+    Console.WriteLine("Susi on peloton = " + peloton);
+    }
+}
 
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Koira koira1 = new Koira();
+        koira1.nimi = "Musti";
+        Console.WriteLine($"{koira1.nimi} syö " + koira1.Syo());
+
+        Koiranpentu pentu1 = new Koiranpentu();
+        pentu1.nimi = "Regina";
+        pentu1.pieni = false;
+        Console.WriteLine($"{pentu1.nimi} juo " + pentu1.Juo());
+
+        Susi susi1 = new Susi(true);
+        susi1.nimi = "Jyrki";
+        // susi1.peloton = true;
+        Console.WriteLine($"{susi1.nimi} haukkuu " + susi1.Haukkuu());
+    }
+}
